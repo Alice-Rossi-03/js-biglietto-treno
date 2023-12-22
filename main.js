da6be203dafe937b16046693820dc1715c2e2bb3
Alice-Rossi-03 <alicerossi0310@gmail.com>
2023-12-22 17:43:30 +0100

    feat (javascript) /algoritmo

--- main.js
+++ main.js
@@ -10,12 +10,27 @@
 // output prezzo finale in forma umana massimo due decimali (due centesimi) 
 
 
+
+
 // primo passo - creazione delle variabili 
 
 let distanceKm = prompt ( "Quanta distanza devi percorrere? (in km)?")
 let clientAge = prompt ( "Inserisci la tua età: (in anni)")
 
-// primo passo - controllo delle variabili 
+// secondo passo - controllo delle variabili 
 
 console.log (distanceKm)
 console.log (clientAge) 
+
+// terzo passo - creazione dell'algoritmo 
+
+if (clientAge < 18){
+    document.writeln((distanceKm * 0.21) - 20/100)
+} else if (clientAge > 65){
+    document.writeln((distanceKm * 0.21) - 40/100)
+} else if (18 < clientAge < 65){
+    document.writeln((distanceKm * 0.21))
+} else {
+    document.writeln("Uhmm error")
+}
+
