361b2e60a7aa7422f124be08b40aeaa313b381ed
Alice-Rossi-03 <alicerossi0310@gmail.com>
2023-12-22 17:28:28 +0100

    feat (javascript) /variabili /console

---
+++ main.js
@@ -0,0 +1,21 @@
+// scomponiamo il problema 
+
+// chiedere il numero dei km 
+// chiedere l'età 
+// calcolare il costo del biglietto del treno 
+    // 0.21 €/km
+    // -20 % minorenni 
+    // -40 % over-65
+    
+// output prezzo finale in forma umana massimo due decimali (due centesimi) 
+
+
+// primo passo - creazione delle variabili 
+
+let distanceKm = prompt ( "Quanta distanza devi percorrere? (in km)?")
+let clientAge = prompt ( "Inserisci la tua età: (in anni)")
+
+// primo passo - controllo delle variabili 
+
+console.log (distanceKm)
+console.log (clientAge) 
